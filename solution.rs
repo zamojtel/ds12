afdf7a6170351014089f3e48399b14390fc5a5a7
Bartek Zamojtel <56871145+zamojtel@users.noreply.github.com>
2026-01-08 02:13:07 +0100

    complete Operation::transform: Insert/Delete, Delete/Insert, Delete/Delete cases

--- solution.rs
+++ solution.rs
@@ -3,6 +3,49 @@ use module_system::Handler;
 
 impl Operation {
     // Add any methods you need.
+    fn transform(&self,other :Operation) -> Operation {
+
+        let result_action = match (&self.action,&other.action) {
+            (Action::Insert { idx: i1, ch: c1 }, Action::Insert { idx: i2, ch: c2 }) => {
+                if i1<i2 || (i1==i2 && self.process_rank<other.process_rank){
+                    Action::Insert { idx: *i1, ch: *c1 }
+                }else{
+                    Action::Insert { idx: *i1+1, ch: *c1 }
+                }
+            },
+            (Action::Insert { idx: i1, ch: c1 }, Action::Delete { idx: i2}) => {
+                if i1 <= i2{
+                    Action::Insert { idx: *i1, ch: *c1 }
+                }else{
+                    Action::Insert { idx: *i1-1, ch: *c1 }
+                }
+            },
+            (Action::Delete { idx: i1 }, Action::Insert { idx: i2, ch: _c2 }) => {
+                // Action::Delete { idx: *i1 }
+                if i2<=i1 {
+                    Action::Delete { idx: *i1+1 }
+                }else{
+                    Action::Delete { idx: *i1 }
+                }
+            },
+            (Action::Delete { idx: i1 },Action::Delete { idx: i2 })=> {
+                if i1 == i2 {
+                    Action::Nop
+                } else if i2 < i1 {
+                    Action::Delete { idx: *i1-1 }
+                } else{
+                    Action::Delete { idx: *i1 }
+                }
+            },
+            (Action::Nop, _) => Action::Nop,
+            (_, Action::Nop) => self.action.clone(),
+        };
+ 
+        Operation{
+            action:result_action,
+            process_rank: self.process_rank
+        }
+    }
 }
 
 /// Process of the system.
