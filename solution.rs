abad46925ad1aba8c1abc71d0b980e1311434c11
Bartek Zamojtel <56871145+zamojtel@users.noreply.github.com>
2026-01-09 21:08:50 +0100

    implement Operation handler

--- solution.rs
+++ solution.rs
@@ -5,7 +5,7 @@ impl Operation {
     // Add any methods you need.
     fn transform(&self,other :Operation) -> Operation {
         let result_action = match (&self.action,&other.action) {
-            (Action::Insert { idx: i1, ch: c1 }, Action::Insert { idx: i2, ch: c2 }) => {
+            (Action::Insert { idx: i1, ch: c1 }, Action::Insert { idx: i2, ch: _c2 }) => {
                 if i1<i2 || (i1==i2 && self.process_rank<other.process_rank){
                     Action::Insert { idx: *i1, ch: *c1 }
                 }else{
@@ -84,7 +84,51 @@ impl<const N: usize> Process<N> {
 #[async_trait::async_trait]
 impl<const N: usize> Handler<Operation> for Process<N> {
     async fn handle(&mut self, msg: Operation) {
-        // todo!("Handle operation issued by other process.");
+        let mut msg_round = 0;
+        let my_round = self.log.len() / N;
+        for l in &self.log {
+            if msg.process_rank == l.process_rank{
+                msg_round+=1;
+            }
+        }
+
+        if msg_round > my_round{
+            self.queue.push(msg);
+        }else if msg_round == my_round {
+            let mut temp_msg = msg;
+            let start = my_round * N;
+            
+            for op in &self.log[start..] {
+                temp_msg = temp_msg.transform(op.clone());
+            }
+
+            self.log.push(temp_msg);
+            let mut i =0;
+            while i < self.queue.len() {
+                let mut current_msg_round = 0;
+                let current_msg = self.queue[i].clone();
+
+                for l in &self.log {
+                    if current_msg.process_rank == l.process_rank{
+                        current_msg_round+=1;   
+                    }
+                }
+                if current_msg_round == self.log.len() /N {
+                    let mut valid_msg = self.queue.remove(i);
+                    let start = current_msg_round * N;
+            
+                    for op in &self.log[start..] {
+                        valid_msg = valid_msg.transform(op.clone());
+                    }
+
+                    self.log.push(valid_msg);
+                }else{
+                    i+=1;
+                }
+            }
+        }else{
+            // we ignore the message
+        }
     }
 }
 
